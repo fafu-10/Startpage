d7096646e3b715bf03e84101a65effa6ac125c28
fafu-10 <59342394+fafu-10@users.noreply.github.com>
2023-08-20 15:30:30 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -47,7 +47,7 @@ searchButton.onclick = search
  * inject bookmarks into html
  */
 
-const bookmarks = [{"id":"vXkXf3mJVT6byCLY","label":"School","bookmarks":[{"id":"BHlt7L1hfyBEkXqr","label":"Login","url":"https://student.myillini.illinois.edu/"},{"id":"NNS1dcvSqZGE8cPr","label":"Outlook","url":"https://outlook.live.com/owa/"},{"id":"e6n4BAzaJgcCLbq7","label":"Moodle","url":"https://learn.illinois.edu/auth/saml2sso/gateway.php"},{"id":"6ykptqFrQKNFb5dw","label":"Canvas","url":"https://canvas.illinois.edu/"}]},{"id":"UX1v2NLi44FGqCXk","label":"Other","bookmarks":[{"id":"WZrnUfYG0aLCXehl","label":"r/unixporn","url":"https://safereddit.com/r/unixporn/"},{"id":"dFJ8d3fNJOjBPYUX","label":"Twitch","url":"https://www.twitch.tv/"},{"id":"tvSexK1zomOxZOw4","label":"Monkeytype","url":"https://monkeytype.com/"},{"id":"t2mCC1D2OZV8ezVm","label":"9Anime","url":"https://9anime.to/home"}]}]
+const bookmarks = [{"id":"vXkXf3mJVT6byCLY","label":"School","bookmarks":[{"id":"BHlt7L1hfyBEkXqr","label":"Login","url":"https://student.myillini.illinois.edu/"},{"id":"NNS1dcvSqZGE8cPr","label":"Outlook","url":"https://outlook.live.com/owa/"},{"id":"e6n4BAzaJgcCLbq7","label":"Moodle","url":"https://learn.illinois.edu/auth/saml2sso/gateway.php"},{"id":"6ykptqFrQKNFb5dw","label":"Canvas","url":"https://canvas.illinois.edu/"}]},{"id":"UX1v2NLi44FGqCXk","label":"Other","bookmarks":[{"id":"WZrnUfYG0aLCXehl","label":"r/unixporn","url":"https://safereddit.com/r/unixporn/"},{"id":"dFJ8d3fNJOjBPYUX","label":"Twitch","url":"https://www.twitch.tv/"},{"id":"tvSexK1zomOxZOw4","label":"Monkeytype","url":"https://monkeytype.com/"},{"id":"t2mCC1D2OZV8ezVm","label":"Aniwave","url":"https://aniwave.to/home"}]}]
 
 const createGroupContainer = () => {
   const container = document.createElement("div")
